de5f4eb9a2f8081a854f106ce89e1c5ff2216d9e
Sebastian Prein <hi@sebastianprein.com>
2018-04-14 14:33:30 +0200

    Add ability to completely hide code preview

--- templates/default/default.php
+++ templates/default/default.php
@@ -7,6 +7,8 @@
     <style><?= $this->read($this->getTheme() ? $this->getTheme() : __DIR__ . '/styles.css') ?></style>
 </head>
 <body>
+    <?php $showCode = $this->isExcerptOnly() === false || $this->getExcerptSize() > 0; ?>
+
     <aside class="side-panel">
         <section class="error-details" data-error>
             <div class="type">
@@ -26,10 +28,10 @@
                 <?php $file = $frame->getFile(); ?>
                 <?php $line = $frame->getLine(); ?>
 
-                <li class="stack-frame<?= $file ? '' : ' -no-file' ?> " data-frame>
+                <li class="stack-frame<?= $file && $showCode ? '' : ' -no-file' ?> " data-frame>
                     <code class="caller php" data-highlight><?= $this->getCaller($frame); ?></code>
 
-                    <?php if ($file): ?>
+                    <?php if ($file && $showCode): ?>
                         <small class="file file-path -plain">
                             <span class="path" data-file-path><?= $file ?></span>
                             <span class="line"><?= $line ?></span>
@@ -45,9 +47,12 @@
     </aside>
 
     <main class="main-panel">
-        <section class="frame-details" data-current-frame>
-            <small class="file-path" data-path></small>
-            <pre class="preview<?= $this->isExcerptOnly() ? ' -excerpt' : '' ?>" data-preview></pre>
+        <?php if ($showCode): ?>
+            <section class="frame-details" data-current-frame>
+                <small class="file-path" data-path></small>
+                <pre class="preview<?= $this->isExcerptOnly() ? ' -excerpt' : '' ?>" data-preview></pre>
+            </section>
+        <?php endif; ?>
         </section>
     </main>
 
