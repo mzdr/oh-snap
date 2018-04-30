17b8f50abcf23dee470d638db087058937369eb1
Sebastian Prein <hi@sebastianprein.com>
2018-04-30 19:51:25 +0200

    Improve visual integration of header and footer panels

--- templates/default/default.php
+++ templates/default/default.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="robots" content="none">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $error->type ?>&hellip;</title>
     <link rel="shortcut icon" href="data:image/png;base64,<?= $this->read(__DIR__ . '/favicon.png', 'base64') ?>">
 
@@ -10,49 +11,49 @@
         <style><?= $this->read($theme) ?></style>
     <?php endforeach; ?>
 </head>
-<body class="<?= $classes($ife($error->hasFrames === false, '-columns')) ?>">
+<body>
     <header class="header-panel"><?= $header ?></header>
 
-    <aside class="side-panel">
-        <section class="error-details" data-error>
-            <div class="type">
-                <i class="icon icon-warning"></i>
-                <span class="text"><?= $error->type ?></span>
-            </div>
-            <h1 class="message"><?= $error->message ?></h1>
-            <pre class="preview" data-preview>
+    <main class="main-panel">
+        <aside class="side-panel">
+            <section class="error-details" data-error>
+                <div class="type">
+                    <i class="icon icon-warning"></i>
+                    <span class="text"><?= $error->type ?></span>
+                </div>
+                <h1 class="message"><?= $error->message ?></h1>
+                <pre class="preview" data-preview>
                 <div class="line-numbers" data-line-numbers data-start="<?= $this->getExcerptStart($error->line) ?>"></div>
                 <code class="code php" data-code><?= $this->getFileContents($error->file, $error->line) ?></code>
             </pre>
-            <small class="file" data-file-path><?= $error->file ?></small>
-        </section>
+                <small class="file" data-file-path><?= $error->file ?></small>
+            </section>
 
-        <?php if ($error->hasFrames): ?>
-        <ul class="call-stack">
-            <?php foreach ($error->frames as $frame): ?>
-                <?php $file = $frame->getFile(); ?>
-                <?php $line = $frame->getLine(); ?>
+            <?php if ($error->hasFrames): ?>
+                <ul class="call-stack">
+                    <?php foreach ($error->frames as $frame): ?>
+                        <?php $file = $frame->getFile(); ?>
+                        <?php $line = $frame->getLine(); ?>
 
-                <li class="<?= $classes('stack-frame', $ife($file && $showCode, '-file')) ?>" data-frame>
-                    <code class="caller php" data-highlight><?= $this->getCaller($frame); ?></code>
+                        <li class="<?= $classes('stack-frame', $ife($file && $showCode, '-file')) ?>" data-frame>
+                            <code class="caller php" data-highlight><?= $this->getCaller($frame); ?></code>
 
-                    <?php if ($file && $showCode): ?>
-                        <small class="file file-path -plain">
-                            <span class="path" data-file-path><?= $file ?></span>
-                            <span class="line"><?= $line ?></span>
-                        </small>
-                        <pre class="preview" data-preview>
+                            <?php if ($file && $showCode): ?>
+                                <small class="file file-path -plain">
+                                    <span class="path" data-file-path><?= $file ?></span>
+                                    <span class="line"><?= $line ?></span>
+                                </small>
+                                <pre class="preview" data-preview>
                             <div class="line-numbers" data-line-numbers data-start="<?= $this->getExcerptStart($line) ?>"></div>
                             <code class="code php" data-code><?= $this->getFileContents($file, $line) ?></code>
                         </pre>
-                    <?php endif; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-        <?php endif; ?>
-    </aside>
+                            <?php endif; ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+        </aside>
 
-    <main class="main-panel">
         <?php if ($showCode): ?>
             <section class="frame-details" data-current-frame>
                 <small class="file-path" data-path></small>
